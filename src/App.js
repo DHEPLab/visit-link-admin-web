9817393c0e796d7ce518e5bda5e9f8750dcc2170
Hooks Zhang <hookszhang@gmail.com>
2020-04-08 11:19:35 +0800

    deps: add antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Form,
+  Select,
+  InputNumber,
+  DatePicker,
+  Switch,
+  Slider,
+  Button,
+  Typography,
+} from 'antd';
 import './App.css';
+
+const { Option } = Select;
+const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section style={{ textAlign: 'center', marginTop: 48, marginBottom: 60 }}>
+        <Title level={2}>
+          <img
+            style={{
+              width: 40,
+              height: 40,
+              marginRight: 12,
+              verticalAlign: 'bottom',
+            }}
+            src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+            alt="Ant Design"
+          />
+          Ant Design
+        </Title>
+      </section>
+      <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+        <Form.Item label="数字输入框">
+          <InputNumber min={1} max={10} defaultValue={3} />
+          <span className="ant-form-text"> 台机器</span>
+          <a href="https://ant.design">链接文字</a>
+        </Form.Item>
+        <Form.Item label="开关">
+          <Switch defaultChecked />
+        </Form.Item>
+        <Form.Item label="滑动输入条">
+          <Slider defaultValue={70} />
+        </Form.Item>
+        <Form.Item label="选择器">
+          <Select defaultValue="lucy" style={{ width: 192 }}>
+            <Option value="jack">jack</Option>
+            <Option value="lucy">lucy</Option>
+            <Option value="disabled" disabled>
+              disabled
+            </Option>
+            <Option value="yiminghe">yiminghe</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="日期选择框">
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button style={{ marginLeft: 8 }}>Cancel</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
