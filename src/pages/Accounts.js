e5f6cd4c1c1bea531543a7a2ded2db5084c3a68b
Hooks Zhang <hookszhang@gmail.com>
2020-04-13 15:45:06 +0800

    feat: load account data

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, Table } from 'antd';
+import styled from 'styled-components';
+
+const columns = [
+  {
+    title: 'Username',
+    dataIndex: 'username',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'realName',
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
 
 export default function () {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axios.get('/api/user').then((response) => {
+      setUser(response.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Account Mgt</h1>
+      <h1>Account Management</h1>
+      <ButtonGroup>
+        <Button type="primary">Add a new account</Button>
+      </ButtonGroup>
+      <Table
+        rowKey="id"
+        dataSource={user.content}
+        columns={columns}
+        pagination={{ current: 1, total: user.totalElements }}
+      />
     </div>
   );
 }
+
+const ButtonGroup = styled.div`
+  padding: 10px 0;
+  text-align: right;
+`;
