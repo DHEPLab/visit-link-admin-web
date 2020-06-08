b461ae7c2d962f274f11db55c6c43940e1e2c0ff
Hooks Zhang <hookszhang@gmail.com>
2020-06-08 16:15:08 +0800

    build: do not package service.conf

--- ansible/package.sh
+++ ansible/package.sh
@@ -5,7 +5,7 @@ set -e -u
 root_dir="$(cd $(dirname $0)/../ && pwd)"
 
 BACKEND_SERVICE=${BACKEND_SERVICE:-0.0.0.0} \
-envsubst < "${root_dir}/etc/nginx/conf.d/service.conf.template" > "${root_dir}/etc/nginx/conf.d/service.conf"
+# envsubst < "${root_dir}/etc/nginx/conf.d/service.conf.template" > "${root_dir}/etc/nginx/conf.d/service.conf"
 
 echo 'Package files into zip file'
 cd "${root_dir}"
