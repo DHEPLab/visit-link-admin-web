a74c99d3bf087bfd7c41d819c2cdc023aff87a21
Hooks Zhang <hookszhang@gmail.com>
2020-06-10 15:50:38 +0800

    feat: assign chw to supervisor

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'antd';
+import { Button, Table } from 'antd';
 import { useParams } from 'react-router-dom';
 import { Card, StaticFormItem } from '../components/*';
 import Axios from 'axios';
@@ -26,7 +26,77 @@ export default function User() {
         <StaticFormItem label="账户密码">******</StaticFormItem>
       </Card>
       <br />
-      <Card title="管理工作人员列表" extra={<Button type="link">分配新工作人员</Button>}></Card>
+      {user.role === 'ROLE_SUPERVISOR' && <AssignChw id={id} />}
     </>
+  );
+}
+
+function AssignChw({ id }) {
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`/admin/user/supervisor/${id}/chw`).then(({ data }) => setDataSource(data));
+  }, []);
+
+  return (
+    <Card title="管理工作人员列表" extra={<Button type="link">分配新工作人员</Button>}>
+      <Table
+        rowKey="id"
+        dataSource={dataSource}
+        columns={[
+          {
+            title: '工作人员姓名',
+            dataIndex: 'realName',
+          },
+          {
+            title: '操作',
+            dataIndex: 'id',
+            width: 200,
+            align: 'center',
+            render() {
+              return <Button type="link">删除</Button>;
+            },
+          },
+        ]}
+      />
+      <NotAssignedChw id={id} />
+    </Card>
+  );
+}
+
+function NotAssignedChw({ id }) {
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`/admin/user/chw/not_assigned`).then(({ data }) => setDataSource(data));
+  }, []);
+
+  function handleAssign(chwId) {
+    Axios.post(`/admin/user/supervisor/${id}/chw`, [chwId]);
+  }
+
+  return (
+    <Table
+      dataSource={dataSource}
+      columns={[
+        {
+          title: '工作人员姓名',
+          dataIndex: 'realName',
+        },
+        {
+          title: '操作',
+          dataIndex: 'id',
+          width: 200,
+          align: 'center',
+          render(chwId) {
+            return (
+              <Button type="link" onClick={() => handleAssign(chwId)}>
+                分配
+              </Button>
+            );
+          },
+        },
+      ]}
+    />
   );
 }
