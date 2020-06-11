ec74c8b0aa3a59512b17c864f414b3651531e9b4
Hooks Zhang <hookszhang@gmail.com>
2020-06-11 14:56:13 +0800

    feat: change password modal

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Modal, Button, Table } from 'antd';
+import { Modal, Button, Table, Input, Space } from 'antd';
 import { useParams } from 'react-router-dom';
 
 import { Card, StaticFormItem } from '../components/*';
@@ -10,6 +10,7 @@ import { Role } from '../constants/enums';
 export default function User() {
   const { id } = useParams();
   const [user, setUser] = useState({});
+  const [changePasswordModalVisible, setChangePasswordModalVisible] = useState(false);
 
   useEffect(() => {
     Axios.get(`/admin/user/${id}`).then(({ data }) => setUser(data));
@@ -24,13 +25,61 @@ export default function User() {
         <StaticFormItem label="权限">{Role[user.role]}</StaticFormItem>
       </Card>
       <br />
-      <Card title="账户信息" extra={<Button type="link">修改密码</Button>}>
+      <Card
+        title="账户信息"
+        extra={
+          <Button type="link" onClick={() => setChangePasswordModalVisible(true)}>
+            修改密码
+          </Button>
+        }
+      >
         <StaticFormItem label="账户名称">{user.username}</StaticFormItem>
         <StaticFormItem label="账户密码">******</StaticFormItem>
       </Card>
       <br />
       {user.role === 'ROLE_SUPERVISOR' && <AssignChw id={id} />}
+      <ChangePasswordModal
+        id={id}
+        visible={changePasswordModalVisible}
+        onCancel={() => setChangePasswordModalVisible(false)}
+      />
     </>
+  );
+}
+
+function ChangePasswordModal({ id, onCancel, ...props }) {
+  const [password, setPassword] = useState();
+
+  function handleSubmit() {
+    Axios.put(`/admin/user/${id}/password`, {
+      password,
+    }).then(onCancel);
+  }
+
+  return (
+    <Modal
+      title="您确定要修改密码吗？"
+      destroyOnClose
+      onCancel={onCancel}
+      footer={
+        <Space>
+          <Button ghost type="primary" onClick={onCancel}>
+            放弃
+          </Button>
+          <Button type="primary" onClick={handleSubmit}>
+            确定
+          </Button>
+        </Space>
+      }
+      {...props}
+    >
+      <p>请您牢记最新修改的密码，提交后将不再显示；且修改后，用户原密码将不可用</p>
+      <Input.Password
+        placeholder="请输入新的账户密码"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+    </Modal>
   );
 }
 
