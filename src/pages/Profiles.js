9b4dfcc97bb7fb9a10e81eaff5147724cc32f9ce
Hooks Zhang <hookszhang@gmail.com>
2020-06-12 12:14:10 +0800

    feat: render profiles use redux state

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+
+import { Role } from '../constants/enums';
 import { Card, StaticFormItem } from '../components/*';
 
-export default function () {
+export default function Profiles() {
+  const { user } = useSelector((state) => state.users);
+
   return (
     <>
       <ContentHeader title="个人中心" />
       <Card title="基本信息">
-        <StaticFormItem label="真实姓名">Personal</StaticFormItem>
-        <StaticFormItem label="联系电话">Personal</StaticFormItem>
-        <StaticFormItem label="权限">Personal</StaticFormItem>
+        <StaticFormItem label="真实姓名">{user.realName}</StaticFormItem>
+        <StaticFormItem label="联系电话">{user.phone}</StaticFormItem>
+        <StaticFormItem label="权限">{Role[user.role]}</StaticFormItem>
       </Card>
       <Card title="账户信息">
-        <StaticFormItem label="账户名称">Personal</StaticFormItem>
+        <StaticFormItem label="账户名称">{user.username}</StaticFormItem>
         <StaticFormItem label="账户密码">******</StaticFormItem>
       </Card>
     </>
