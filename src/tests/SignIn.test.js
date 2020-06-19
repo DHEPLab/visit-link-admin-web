71a52f6faafd0258d295aea97ffa52fb2c510c09
Hooks Zhang <hookszhang@gmail.com>
2020-06-19 11:01:45 +0800

    fix: sign in test

--- src/tests/SignIn.test.js
+++ src/tests/SignIn.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import setup from './setup';
 import SignIn from '../pages/SignIn';
 
-test('renders text sign in page', () => {
+test('renders button sign in page', () => {
   const { getByText } = setup(<SignIn />);
-  const text = getByText(/Healthy Future/i);
+  const text = getByText('登 录');
   expect(text).toBeInTheDocument();
 });
