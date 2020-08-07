b387a6c059f6fd0c225eb7c0c988e55d47f9b495
Hooks Zhang <hookszhang@gmail.com>
2020-08-07 16:46:35 +0800

    feat: add unreviewed tab to Babies

--- src/pages/Babies.js
+++ src/pages/Babies.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 import moment from 'moment';
+import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { Button, Space, Tabs } from 'antd';
 
@@ -17,8 +18,9 @@ import {
 
 const { TabPane } = Tabs;
 
-function Babies({ loadData, onChangeSearch, ...props }) {
+export default function Babies() {
   const history = useHistory();
+  const [tab, setTab] = useState('approved');
   const [visible, openBaby, closeBaby] = useBoolState(false);
 
   function handleCreateBaby(values) {
@@ -26,81 +28,32 @@ function Babies({ loadData, onChangeSearch, ...props }) {
     values.birthday = values.birthday && moment(values.birthday).format('YYYY-MM-DD');
     values.edc = values.edc && moment(values.edc).format('YYYY-MM-DD');
     Axios.post('/admin/babies', values).then(() => {
-      loadData();
+      refresh();
       closeBaby();
     });
+  }
+
+  // change tab to refresh table
+  function refresh() {
+    const origin = tab;
+    setTab(Math.random());
+    setTab(origin);
   }
 
   return (
     <>
       <ContentHeader title="宝宝管理">
-        <Space size="large">
-          <SearchInput
-            className="master"
-            placeholder="请输入宝宝姓名、ID或所在区域搜索"
-            onChange={(e) => onChangeSearch('search', e.target.value)}
-          />
-          {/* <Button ghost type="primary">
-            批量创建宝宝
-          </Button> */}
-          <Button type="primary" onClick={openBaby}>
-            创建新宝宝
-          </Button>
-        </Space>
+        <Button type="primary" onClick={openBaby}>
+          创建新宝宝
+        </Button>
       </ContentHeader>
 
-      <CardTabs>
-        <TabPane tab="已审核">
-          <ZebraTable
-            {...props}
-            rowKey="id"
-            className="clickable"
-            onRow={(record) => ({
-              onClick: () => {
-                history.push(`/babies/${record.id}`);
-              },
-            })}
-            columns={[
-              {
-                title: '宝宝姓名',
-                dataIndex: 'name',
-                align: 'center',
-                width: 120,
-              },
-              {
-                title: 'ID',
-                width: 200,
-                dataIndex: 'identity',
-              },
-              {
-                title: '性别',
-                width: 80,
-                dataIndex: 'gender',
-                render: (h) => Gender[h],
-              },
-              {
-                title: '所在区域',
-                dataIndex: 'area',
-                width: 300,
-              },
-              {
-                title: '负责社区工作者',
-                dataIndex: 'chw',
-                width: 150,
-              },
-              {
-                title: '已上课堂',
-                dataIndex: 'visitCount',
-                width: 150,
-                render: (h) => `${h} 节课堂`,
-              },
-              {
-                title: '当前进度',
-                dataIndex: 'currentLessonName',
-                width: 200,
-              },
-            ]}
-          />
+      <CardTabs onChange={setTab}>
+        <TabPane tab="已审核" key="approved">
+          <PageApproved history={history} />
+        </TabPane>
+        <TabPane tab="待审核" key="unreviewed">
+          <PageUnreviewed history={history} />
         </TabPane>
       </CardTabs>
 
@@ -115,4 +68,86 @@ function Babies({ loadData, onChangeSearch, ...props }) {
   );
 }
 
-export default WithPage(Babies, '/admin/babies');
+const PageApproved = WithPage(Approved, '/admin/babies');
+const PageUnreviewed = WithPage(Unreviewed, '/admin/babies');
+
+function Unreviewed() {
+  return <></>;
+}
+
+function Approved({ history, loadData, onChangeSearch, ...props }) {
+  return (
+    <>
+      <ApprovedBar>
+        <Space size="large">
+          <SearchInput
+            className="master"
+            placeholder="请输入宝宝姓名、ID或所在区域搜索"
+            onChange={(e) => onChangeSearch('search', e.target.value)}
+          />
+        </Space>
+      </ApprovedBar>
+      <ZebraTable
+        {...props}
+        rowKey="id"
+        className="clickable"
+        onRow={(record) => ({
+          onClick: () => {
+            history.push(`/babies/${record.id}`);
+          },
+        })}
+        columns={[
+          {
+            title: '宝宝姓名',
+            dataIndex: 'name',
+            align: 'center',
+            width: 120,
+          },
+          {
+            title: 'ID',
+            width: 200,
+            dataIndex: 'identity',
+          },
+          {
+            title: '性别',
+            width: 80,
+            dataIndex: 'gender',
+            render: (h) => Gender[h],
+          },
+          {
+            title: '所在区域',
+            dataIndex: 'area',
+            width: 300,
+          },
+          {
+            title: '负责社区工作者',
+            dataIndex: 'chw',
+            width: 150,
+          },
+          {
+            title: '已上课堂',
+            dataIndex: 'visitCount',
+            width: 150,
+            render: (h) => `${h} 节课堂`,
+          },
+          {
+            title: '当前进度',
+            dataIndex: 'currentLessonName',
+            width: 200,
+          },
+        ]}
+      />
+    </>
+  );
+}
+
+const ApprovedBar = styled.div`
+  height: 76px;
+  padding-left: 30px;
+  padding-right: 20px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #ffc3a0;
+`;
