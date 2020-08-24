f51d581a8aaa55a6ddecbcf1005c9fe224bc9587
Hooks Zhang <hookszhang@gmail.com>
2020-08-24 14:08:23 +0800

    feat: delete tags

--- src/components/TagSelect.js
+++ src/components/TagSelect.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import Axios from 'axios';
+import styled from 'styled-components';
 
-import { Select } from 'antd';
+import { Select, Button } from 'antd';
 import { useManualFetch } from '../utils';
 
-export default function TagSelect(props) {
+export default function TagSelect({ onChange, ...props }) {
   const [options, refresh] = useManualFetch('/admin/tags', {}, []);
 
+  function handleDelete(option) {
+    setTimeout(() => {
+      // delete database tag
+      Axios.delete(`/admin/tags/${option.id}`).then(() => refresh());
+      // delete value from props.value
+      onChange(props.value?.filter((v) => v !== option.name));
+    }, 20);
+    return false;
+  }
+
   return (
-    <Select mode="tags" onFocus={() => refresh()} {...props}>
+    <Select
+      mode="tags"
+      onFocus={() => refresh()}
+      optionLabelProp="name"
+      onChange={onChange}
+      {...props}
+    >
       {options.map((option) => (
         <Select.Option key={option.id} value={option.name}>
-          {option.name}
+          <Option>
+            {option.name}
+            {!props.value?.includes(option.name) && (
+              <Button size="small" type="link" onClick={() => handleDelete(option)}>
+                删除
+              </Button>
+            )}
+          </Option>
         </Select.Option>
       ))}
     </Select>
   );
 }
+
+const Option = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
