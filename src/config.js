bce2157d021b4d6e29bbf94d6950c51ae731d04e
黄文星 <huangwenxing@aidootech.com>
2020-09-04 13:59:13 +0800

    simple tips

--- src/config.js
+++ src/config.js
@@ -8,27 +8,27 @@ import { Message } from './components/*';
 const urlInfo = {
   get: [],
   post: [
-    { url: "/admin/curriculums/draft", isequals: true, title: "操作成功", context: "草稿保存成功" },
-    { url: "/admin/curriculums/", isequals: false, title: "操作成功", context: "添加成功", endsWith: "babies" },//大纲分配宝宝列表添加新宝宝
+    { url: "/admin/curriculums/draft", isequals: true, title: "草稿保存成功", context: "" },
+    { url: "/admin/curriculums/", isequals: false, title: "添加成功", context: "", endsWith: "babies" },//大纲分配宝宝列表添加新宝宝
     { url: "/admin/curriculums", isequals: true, title: "发布成功", context: "大纲已发布，可以添加宝宝后在app端查看" },
-    { url: "/admin/modules/draft", isequals: true, title: "操作成功", context: "草稿保存成功" },
+    { url: "/admin/modules/draft", isequals: true, title: "草稿保存成功", context: "" },
     { url: "/admin/modules", isequals: true, title: "发布成功", context: "模块已发布，可在课堂编辑时关联此模块" },
   ],
   put: [
-    { url: "/admin/curriculums/draft", isequals: true, title: "操作成功", context: "草稿保存成功" },
-    { url: "/admin/users/", isequals: false, title: "操作成功", context: "密码修改成功", endsWith: "/password" },
-    { url: "/admin/users/", isequals: false, title: "操作成功", context: "保存成功", endsWith: "" },
-    { url: "/admin/modules/draft", isequals: true, title: "操作成功", context: "草稿保存成功" },
+    { url: "/admin/curriculums/draft", isequals: true, title: "草稿保存成功", context: "" },
+    { url: "/admin/users/", isequals: false, title: "密码修改成功", context: "", endsWith: "/password" },
+    { url: "/admin/users/", isequals: false, title: "保存成功", context: "", endsWith: "" },
+    { url: "/admin/modules/draft", isequals: true, title: "草稿保存成功", context: "" },
     { url: "/admin/curriculums", isequals: true, title: "发布成功", context: "大纲已发布，可以添加宝宝后在app端查看" },
     { url: "/admin/modules", isequals: true, title: "发布成功", context: "模块已发布，可在课堂编辑时关联此模块" },
   ],
   delete: [
-    { url: "/admin/carers/", isequals: false, title: "操作成功", context: "删除成功", endsWith: "" },//删除看护人
-    { url: "/admin/curriculums/", isequals: false, title: "操作成功", context: "删除成功", endsWith: "" },//删除大纲
-    { url: "/admin/modules/", isequals: false, title: "操作成功", context: "删除成功", endsWith: "" },//删除模块
-    { url: "/admin/babies/", isequals: false, title: "操作成功", context: "删除成功", endsWith: "curriculum" },//大纲分配宝宝列表删除宝宝
-    { url: "/admin/babies/", isequals: false, title: "操作成功", context: "解绑成功", endsWith: "chw" },//社区工作者解绑宝宝
-    { url: "/admin/users/chw/", isequals: false, title: "操作成功", context: "解绑成功", endsWith: "supervisor" },//负责社区工作者列表解绑社区工作者
+    { url: "/admin/carers/", isequals: false, title: "删除成功", context: "", endsWith: "" },//删除看护人
+    { url: "/admin/curriculums/", isequals: false, title: "删除成功", context: "", endsWith: "" },//删除大纲
+    { url: "/admin/modules/", isequals: false, title: "删除成功", context: "", endsWith: "" },//删除模块
+    { url: "/admin/babies/", isequals: false, title: "删除成功", context: "", endsWith: "curriculum" },//大纲分配宝宝列表删除宝宝
+    { url: "/admin/babies/", isequals: false, title: "解绑成功", context: "", endsWith: "chw" },//社区工作者解绑宝宝
+    { url: "/admin/users/chw/", isequals: false, title: "解绑成功", context: "", endsWith: "supervisor" },//负责社区工作者列表解绑社区工作者
   ]
 }
 
@@ -78,12 +78,10 @@ Axios.interceptors.response.use(
 function overallSituationTips(method, url) {
   let infoArray = urlInfo[method]
   let result = infoArray.filter(e => (e["isequals"] && url === e.url) || (!e["isequals"] && url.includes(e.url)));
-  console.log("拿到头结果", result)
   if (result && result.length === 1) {
     Message.success(result[0].title, result[0].context);
   } else if (result && result.length > 1) {
     let res = result.filter(e => (!e["isequals"] && url.endsWith(e["endsWith"])) || e["isequals"] === true);
-    console.log(res, 88888)
     if (res && res.length > 0) {
       Message.success(res[0].title, res[0].context);
     }
