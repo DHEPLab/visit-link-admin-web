802ba9d8cb6b48ef63caca9c41bd4c3adb6506e8
Huey Zhang <hueyzng@gmail.com>
2020-11-16 14:10:46 +0800

    feat: scroll to follow when moving components down.

--- src/components/curriculum/ComponentField.js
+++ src/components/curriculum/ComponentField.js
@@ -3,7 +3,7 @@ import { FastField } from "formik";
 
 import { Text, Media, Switch, PageFooter } from "./*";
 
-export default function ComponentField({ name, component, index, ...props }) {
+export default function ComponentField({ name, component, index, onMoveDown, ...props }) {
   let As;
   switch (component.type) {
     case "Text":
@@ -21,7 +21,25 @@ export default function ComponentField({ name, component, index, ...props }) {
     default:
       As = <div>{component.type}</div>;
   }
+
   // as can either be a React component or the name of an HTML element to render.
   // Formik will automagically inject onChange, onBlur, name, and value props of the field designated by the name prop to the (custom) component.
-  return <FastField name={`${name}.${index}.value`} {...props} as={As} />;
+  return (
+    <div id={`${name}.${index}`}>
+      <FastField
+        name={`${name}.${index}.value`}
+        onMoveDown={() => {
+          onMoveDown();
+          // Scroll to follow when moving components down.
+          // When moving up it is auto-followed and not processed, possibly due to React re-rendering? I don't know.
+          setTimeout(() => {
+            const next = document.getElementById(`${name}.${index + 1}`);
+            next && next.scrollIntoView();
+          }, 200);
+        }}
+        {...props}
+        as={As}
+      />
+    </div>
+  );
 }
