b174737c32731289b94e5d08f94c391735abdfab
jiangbangfa <976951401@qq.com>
2022-05-16 15:25:59 +0800

    修复：时间上午下午显示的问题

--- src/utils/visit.js
+++ src/utils/visit.js
@@ -20,13 +20,14 @@ function defaultDatetime(range, visitTime) {
 }
 
 function meridiem(momentInstance) {
-  const hour = momentInstance.hour();
+  let hour = momentInstance.hour();
   const minute = momentInstance.minute();
+  hour += (minute / 60)
   if (hour < 9) {
     return "早上";
-  } else if (hour < 11 && minute < 30) {
+  } else if (hour < 11.5) {
     return "上午";
-  } else if (hour < 13 && minute < 30) {
+  } else if (hour < 13.5) {
     return "中午";
   } else if (hour < 18) {
     return "下午";
