bc75724984406e95242ec6d89b628639808e723c
tw-haoranli <haoran.li1@thoughtworks.com>
2024-07-25 16:36:02 +0800

    chore: add i18n language for account management

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -5,7 +5,9 @@ import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Form, Button, Tabs, Radio, Input } from "antd";
 import { useQueryParam, StringParam } from "use-query-params";
+import { useTranslation } from 'react-i18next';
 
+import i18next from '../i18n';
 import Rules from "../constants/rules";
 import { useBoolState } from "../utils";
 import { Role } from "../constants/enums";
@@ -13,6 +15,7 @@ import { ModalForm, WithPage, ContentHeader, CardTabs, ZebraTable, SearchInput, 
 
 const { TabPane } = Tabs;
 export default function Users() {
+  const { t } = useTranslation(["users", "common"]);
   const history = useHistory();
   const [tab, setTab] = useQueryParam("tab", StringParam);
   const [visible, openUser, closeUser] = useBoolState();
@@ -39,23 +42,23 @@ export default function Users() {
 
   return (
     <>
-      <ContentHeader title="账户管理">
+      <ContentHeader title={t('accountManagement')}>
         <Button type="primary" onClick={openUser}>
-          创建新用户
+          {t('createNewUser')}
         </Button>
       </ContentHeader>
 
       {user.id && (
         <CardTabs onChange={setTab} defaultActiveKey={tab}>
-          <TabPane tab="社区工作者" key="chw">
+          <TabPane tab={t('chw')} key="chw">
             <PageCHW tab={tab} history={history} />
           </TabPane>
           {isAdmin && (
             <>
-              <TabPane tab="督导员" key="supervisor">
+              <TabPane tab={t('supervisor')} key="supervisor">
                 <PageSupervisor tab={tab} history={history} />
               </TabPane>
-              <TabPane tab="管理员" key="admin">
+              <TabPane tab={t('admin')} key="admin">
                 <PageAdmin tab={tab} history={history} />
               </TabPane>
             </>
@@ -64,14 +67,14 @@ export default function Users() {
       )}
 
       <ModalForm
-        title="创建新用户"
+        title={t('createNewUser')}
         visible={visible}
         onCancel={closeUser}
         onFinish={handleCreateUser}
         initialValues={{ role: "ROLE_CHW" }}
       >
-        <h3>用户信息</h3>
-        <Form.Item label="权限" name="role" rules={Rules.Required}>
+        <h3>{t('generalInformation')}</h3>
+        <Form.Item label={t('permissions')} name="role" rules={Rules.Required}>
           <Radio.Group>
             {Object.keys(Role)
               .filter((key) => {
@@ -85,7 +88,7 @@ export default function Users() {
               ))}
           </Radio.Group>
         </Form.Item>
-        <Form.Item label="真实姓名" name="realName" rules={Rules.RealName}>
+        <Form.Item label={t('name')} name="realName" rules={Rules.RealName}>
           <Input autoFocus />
         </Form.Item>
         <Form.Item noStyle shouldUpdate={(old, curr) => old.role !== curr.role}>
@@ -96,7 +99,7 @@ export default function Users() {
                   <Form.Item label="ID" name={["chw", "identity"]} rules={Rules.Required}>
                     <Input />
                   </Form.Item>
-                  <Form.Item label="所在区域" name={["chw", "tags"]} rules={Rules.Area}>
+                  <Form.Item label={t('area')} name={["chw", "tags"]} rules={Rules.Area}>
                     <ChwTagSelector />
                   </Form.Item>
                 </>
@@ -104,14 +107,14 @@ export default function Users() {
             </>
           )}
         </Form.Item>
-        <Form.Item label="联系电话" name="phone" rules={Rules.Phone}>
+        <Form.Item label={t('phone')} name="phone" rules={Rules.Phone}>
           <Input />
         </Form.Item>
-        <h3>账户信息</h3>
-        <Form.Item label="账户名称" name="username" rules={Rules.Required}>
+        <h3>{t('accountInformation')}</h3>
+        <Form.Item label={t('username')} name="username" rules={Rules.Required}>
           <Input />
         </Form.Item>
-        <Form.Item label="账户密码" name="password" rules={Rules.Password}>
+        <Form.Item label={t('password')} name="password" rules={Rules.Password}>
           <Input.Password />
         </Form.Item>
       </ModalForm>
@@ -124,6 +127,7 @@ const PageSupervisor = WithPage(Supervisor, "/admin/users/supervisor", {}, false
 const PageAdmin = WithPage(Admin, "/admin/users/admin?sort=id,desc", {}, false);
 
 function CHW({ tab, history, loadData, onChangeSearch, ...props }) {
+  const { t } = useTranslation(["users", "common"]);
   useEffect(() => {
     tab === "chw" && loadData();
   }, [tab, loadData]);
@@ -134,7 +138,7 @@ function CHW({ tab, history, loadData, onChangeSearch, ...props }) {
         <SearchInput
           style={{ width: "420px" }}
           onChange={(e) => onChangeSearch("search", e.target.value)}
-          placeholder="请输入社区工作者姓名、ID或所在区域搜索"
+          placeholder={t('searchChwPlaceholder')}
         />
         {/* <Button ghost type="primary">
           批量创建社区工作者
@@ -148,27 +152,27 @@ function CHW({ tab, history, loadData, onChangeSearch, ...props }) {
         columns={[
           realName,
           {
-            title: "ID",
+            title: t('id'),
             width: 150,
             dataIndex: ["user", "chw", "identity"],
           },
           {
-            title: "所在区域",
+            title: t('area'),
             width: 350,
             dataIndex: ["user", "chw", "tags"],
             render: (tags) => tags && tags.join(", "),
           },
           phone,
           {
-            title: "督导员",
+            title: t('supervisor'),
             width: 120,
             dataIndex: ["user", "chw", "supervisor", "realName"],
           },
           {
-            title: "负责宝宝",
+            title: t('babyCount'),
             width: 100,
             dataIndex: "babyCount",
-            render: (h) => `${h} 位`,
+            render: (h) => `${h} ${t('unit.person', { ns: 'common' })}`,
           },
           username,
         ]}
@@ -189,6 +193,7 @@ const ChwBar = styled.div`
 `;
 
 function Supervisor({ tab, history, loadData, ...props }) {
+  const { t } = useTranslation(["users", "common"]);
   useEffect(() => {
     tab === "supervisor" && loadData();
   }, [tab, loadData]);
@@ -204,10 +209,10 @@ function Supervisor({ tab, history, loadData, ...props }) {
           realName,
           phone,
           {
-            title: "负责社区工作者",
+            title: t('chw'),
             dataIndex: "chwCount",
             width: 200,
-            render: (h) => `${h} 位`,
+            render: (h) => `${h} ${t('unit.person', { ns: 'common' })}`,
           },
           username,
         ]}
@@ -239,20 +244,20 @@ function Admin({ tab, history, loadData, ...props }) {
 }
 
 const realName = {
-  title: "姓名",
+  title: i18next.t('name', { ns: "users" }),
   align: "center",
   width: 100,
   dataIndex: ["user", "realName"],
 };
 
 const phone = {
-  title: "联系电话",
+  title: i18next.t('phone', { ns: "users" }),
   width: 200,
   dataIndex: ["user", "phone"],
 };
 
 const username = {
-  title: "账户名称",
+  title: i18next.t('username', { ns: "users" }),
   dataIndex: ["user", "username"],
   width: 200,
 };
