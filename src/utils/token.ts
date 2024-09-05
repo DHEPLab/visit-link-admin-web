ef7f2c83e7f2a086b801372c1af050c85f828377
Tang Zhen <cloudztang@gmail.com>
2024-09-05 18:40:29 +0800

    [HF-36][Zhen Tang] chore: convert token.js to ts

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const KEY = "VISIT_LINK_JWT";
 
-export function applyToken(token) {
+export function applyToken(token?: string | null): void {
   if (token) {
     localStorage.setItem(KEY, token);
     axios.defaults.headers["Authorization"] = "Bearer " + token;
   }
 }
 
-export function getToken() {
+export function getToken(): string | null {
   return localStorage.getItem(KEY);
 }
 
-export function clearToken() {
+export function clearToken(): void {
   localStorage.setItem(KEY, "");
   axios.defaults.headers["Authorization"] = "";
 }
