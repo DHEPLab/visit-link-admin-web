f22767b1d0b3479ac02677645ceca3835180898b
Tang Zhen <cloudztang@gmail.com>
2024-09-08 10:44:39 +0800

    [HF-36][Zhen Tang] chore: convert setup.jsx to ts

--- src/tests/setup.tsx
+++ src/tests/setup.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
-export default function testSetup(component, initialEntries = ["/"]) {
+export default function testSetup(component: React.ReactNode, initialEntries = ["/"]) {
   return {
     ...render(<MemoryRouter initialEntries={initialEntries}>{component}</MemoryRouter>),
     history,
