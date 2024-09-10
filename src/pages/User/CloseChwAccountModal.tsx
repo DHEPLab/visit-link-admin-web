c49cebaccd46b353ca5d3ad4cfdbeb72e80ededb
Yuheng Zhou <yuheng.zhou@thoughtworks.com>
2024-09-10 17:04:24 +0800

    [HF-74][Yuheng Zhou] fix: fix selector

--- src/pages/User/CloseChwAccountModal.tsx
+++ src/pages/User/CloseChwAccountModal.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import Rules from "@/constants/rules";
 import { Page } from "@/models/res/Page";
 import { ChwUser } from "@/models/res/User";
+import styled from "@emotion/styled";
 
 interface CloseChwAccountModalProps {
   id?: string;
@@ -14,6 +15,12 @@ interface CloseChwAccountModalProps {
   onCancel: MouseEventHandler;
   onFinish: (values: { userId: number }) => void;
 }
+
+const CustomSelectorWrapper = styled.div`
+  .ant-select.baby-transfer-selector .ant-select-selector {
+    width: 100%;
+  }
+`;
 
 const CloseChwAccountModal: React.FC<CloseChwAccountModalProps> = ({
   id,
@@ -67,25 +74,28 @@ const CloseChwAccountModal: React.FC<CloseChwAccountModalProps> = ({
       </p>
       <Form form={form} onFinish={onFinish} labelCol={{ span: 0 }}>
         {!isBabiesEmpty && (
-          <Form.Item label={t("chw")} name="userId" rules={Rules.Required}>
-            <Select
-              showSearch
-              filterOption={false}
-              onFocus={() => debounceSearch()}
-              onSearch={debounceSearch}
-              style={{ width: "100%" }}
-              placeholder={t("selectBabyPlaceholder")}
-            >
-              {options
-                .filter((o) => o.user.id !== Number(id))
-                .map((o) => (
-                  <Select.Option key={o.user.id}>
-                    {o.user.realName}/{o.user.chw?.identity}/{(o.user.chw?.tags || []).join(",")} // TODO: fix the lang
-                    en tags
-                  </Select.Option>
-                ))}
-            </Select>
-          </Form.Item>
+          <CustomSelectorWrapper>
+            <Form.Item label={t("chw")} name="userId" rules={Rules.Required}>
+              <Select
+                showSearch
+                filterOption={false}
+                onFocus={() => debounceSearch()}
+                onSearch={debounceSearch}
+                style={{ width: "100%" }}
+                className={"baby-transfer-selector"}
+                placeholder={t("selectBabyPlaceholder")}
+              >
+                {options
+                  .filter((o) => o.user.id !== Number(id))
+                  .map((o) => (
+                    <Select.Option key={o.user.id}>
+                      {o.user.realName}/{o.user.chw?.identity}/{(o.user.chw?.tags || []).join(",")} // TODO: fix the
+                      lang en tags
+                    </Select.Option>
+                  ))}
+              </Select>
+            </Form.Item>
+          </CustomSelectorWrapper>
         )}
       </Form>
     </Modal>
