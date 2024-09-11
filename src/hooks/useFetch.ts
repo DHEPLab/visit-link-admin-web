a5ea1ab150b788c672fc5df1e1100c44b8fde1fc
Tang Zhen <cloudztang@gmail.com>
2024-09-11 15:22:09 +0800

    [HF-64][Zhen Tang] fix: make the useFetch can handle the abort signal

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -7,24 +7,34 @@ export default function useFetch<T>(
   initialState: T = {} as T,
   manualFetch = false,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): [T, (search?: any) => void] {
+): [T, (search?: any, signal?: AbortSignal) => void] {
   const [data, setData] = useState<T>(initialState);
 
-  function load(search = {}) {
+  function load(search = {}, signal?: AbortSignal) {
     axios
       .get<T>(url, {
         params: {
           ...params,
           ...search,
         },
+        signal,
       })
-      .then((r) => setData(r.data));
+      .then((r) => setData(r.data))
+      .catch((err) => {
+        if (!axios.isCancel(err)) {
+          return Promise.reject(err);
+        }
+      });
   }
 
   useEffect(() => {
+    const abortController = new AbortController();
+
     if (!manualFetch) {
-      load();
+      load({}, abortController.signal);
     }
+
+    return () => abortController.abort();
   }, []);
   return [data, load];
 }
