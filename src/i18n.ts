6a253144d14b552ca9733712b8ba80a62a234555
Tang Zhen <cloudztang@gmail.com>
2024-09-14 16:40:48 +0800

    [HF-64][Zhen Tang] chore: convert translation file to ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,19 +4,22 @@ import enTranslation from "./locales/en/translation";
 import zhTranslation from "./locales/zh/translation";
 import LngDetector from "i18next-browser-languagedetector";
 
+export const defaultNS = "common";
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
+export const resources = {
   en: enTranslation,
   zh: zhTranslation,
-};
+} as const;
 
 await i18n
   .use(initReactI18next)
   .use(LngDetector)
   .init({
+    defaultNS,
     resources,
+    debug: false,
     interpolation: {
       escapeValue: false,
     },
