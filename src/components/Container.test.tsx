e6118c318f105b597fee4f6a83f43ea2b090493d
Tang Zhen <cloudztang@gmail.com>
2024-09-14 17:05:15 +0800

    [HF-64][Zhen Tang] chore: correct the test assert

--- src/components/Container.test.tsx
+++ src/components/Container.test.tsx
@@ -4,6 +4,6 @@ import Container from "./Container";
 
 test("should render readonly Container", () => {
   const { queryByText, queryByTestId } = render(<Container icon="icontext" readonly />);
-  expect(queryByText(/移除/)).not.toBeInTheDocument();
+  expect(queryByText(/Remove/)).not.toBeInTheDocument();
   expect(queryByTestId("move")).not.toBeInTheDocument();
 });
