06486642c64f2eb36efa422411416bbd238e8536
Tang Zhen <cloudztang@gmail.com>
2024-09-19 11:04:11 +0800

    [HF-93][Zhen Tang] fix: fix issue when navigate to edit mode, the name and description field value are not set

--- src/pages/Curriculum/Curriculum.tsx
+++ src/pages/Curriculum/Curriculum.tsx
@@ -70,7 +70,7 @@ const Curriculum: React.FC = () => {
       });
 
     return () => abortController.abort();
-  }, []);
+  }, [readonly]);
 
   function submitDraft() {
     setSubmitURL("/admin/curriculums/draft");
